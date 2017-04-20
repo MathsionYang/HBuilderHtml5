a0f768db4da6a2a93aa1d1413682c07c4a818d97
lizy <77211356@qq.com>
2017-04-20 15:50:52 +0800

    交通路线查询三种方法
    加了输入提示，与样式

--- js/BaiduMapAPI.js
+++ js/BaiduMapAPI.js
@@ -6,7 +6,117 @@ function init(){
 	map.addControl(new BMap.MapTypeControl());   //添加地图类型控件
 	map.setCurrentCity("武汉");          // 设置地图显示的城市 此项是必须设置的
 	map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+	
+	
+	//-------------地图样式控制---------------------------------
+	//初始化模板选择的下拉框
+	var sel = document.getElementById('stylelist');
+	for(var key in mapstyles){
+		var style = mapstyles[key];
+		var item = new  Option(style.title,key);
+		sel.options.add(item);
+	}
+	//window.map = map;
+	changeMapStyle('normal')
+	sel.value = 'normal';
+	
+	//--------------------输入框自动提示控制--------------------------
+	
+	//-----------------------起点-------------------------------------------------------------------------------------------
+	var aa = new BMap.Autocomplete(    //建立一个自动完成的对象
+		{"input" : "tex_a"
+		,"location" : map
+	});
+	
+	aa.addEventListener("onhighlight", function(e) {  //鼠标放在下拉列表上的事件
+	var str = "";
+		var _value = e.fromitem.value;
+		var value = "";
+		if (e.fromitem.index > -1) {
+			value = _value.province +  _value.city +  _value.district +  _value.street +  _value.business;
+		}    
+		str = "FromItem<br />index = " + e.fromitem.index + "<br />value = " + value;
+		
+		value = "";
+		if (e.toitem.index > -1) {
+			_value = e.toitem.value;
+			value = _value.province +  _value.city +  _value.district +  _value.street +  _value.business;
+		}    
+		str += "<br />ToItem<br />index = " + e.toitem.index + "<br />value = " + value;
+		G("searchResultPanel").innerHTML = str;
+	});
+
+	var myValue;
+	aa.addEventListener("onconfirm", function(e) {    //鼠标点击下拉列表后的事件
+	var _value = e.item.value;
+		myValue = _value.province +  _value.city +  _value.district +  _value.street +  _value.business;
+		G("searchResultPanel").innerHTML ="onconfirm<br />index = " + e.item.index + "<br />myValue = " + myValue;
+		
+		setPlace();
+	});
+	//------------------------------------------------------------------------------------------------------------------
+	//----------------------------终点--------------------------------------------------------------------------------------
+	var ab = new BMap.Autocomplete(    //建立一个自动完成的对象
+		{"input" : "tex_b"
+		,"location" : map
+	});
+	
+	ab.addEventListener("onhighlight", function(e) {  //鼠标放在下拉列表上的事件
+	var str = "";
+		var _value = e.fromitem.value;
+		var value = "";
+		if (e.fromitem.index > -1) {
+			value = _value.province +  _value.city +  _value.district +  _value.street +  _value.business;
+		}    
+		str = "FromItem<br />index = " + e.fromitem.index + "<br />value = " + value;
+		
+		value = "";
+		if (e.toitem.index > -1) {
+			_value = e.toitem.value;
+			value = _value.province +  _value.city +  _value.district +  _value.street +  _value.business;
+		}    
+		str += "<br />ToItem<br />index = " + e.toitem.index + "<br />value = " + value;
+		G("searchResultPanel").innerHTML = str;
+	});
+
+	var myValue;
+	ab.addEventListener("onconfirm", function(e) {    //鼠标点击下拉列表后的事件
+	var _value = e.item.value;
+		myValue = _value.province +  _value.city +  _value.district +  _value.street +  _value.business;
+		G("searchResultPanel").innerHTML ="onconfirm<br />index = " + e.item.index + "<br />myValue = " + myValue;
+		
+		setPlace();
+	});
+	//------------------------------------------------------------------------------------------------------------------
+	
+	//搜索实时定位
+	function setPlace(){
+		map.clearOverlays();    //清除地图上所有覆盖物
+		function myFun(){
+			var pp = local.getResults().getPoi(0).point;    //获取第一个智能搜索的结果
+			map.centerAndZoom(pp, 18);
+			map.addOverlay(new BMap.Marker(pp));    //添加标注
+		}
+		var local = new BMap.LocalSearch(map, { //智能搜索
+		  onSearchComplete: myFun
+		});
+		local.search(myValue);
+	}
 }
+
+//改变地图样式
+function changeMapStyle(style){
+	map.setMapStyle({style:style});
+	$('#desc').html(mapstyles[style].desc);
+}
+
+//输入提示标签值返回
+function G(id) {
+	return document.getElementById(id);
+}
+
+
+
 //步行规划函数
 function WalkRouteQuery(){
 	alert("这里是测试是否跳进了这个函数里，OK!");
@@ -34,3 +144,7 @@ function BusQuery(){
 	});
     transit.search(a, b);
 }
+
+
+	
+
