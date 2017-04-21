14f9b18a126e3f1401cf957416812ca3260339d4
lizy <77211356@qq.com>
2017-04-21 15:48:45 +0800

    addmaker
    添加标记点

--- js/BaiduMapAPI.js
+++ js/BaiduMapAPI.js
@@ -2,7 +2,7 @@ var map;//定义地图全局变量
 function init(){
 	// 百度地图API功能
 	map = new BMap.Map("baidumap");    // 创建Map实例
-	map.centerAndZoom(new BMap.Point(114.353622,30.56486), 15);  // 初始化地图,设置中心点坐标和地图级别
+	map.centerAndZoom(new BMap.Point(114.353622,30.56486), 5);  // 初始化地图,设置中心点坐标和地图级别
 	map.addControl(new BMap.MapTypeControl());   //添加地图类型控件
 	map.setCurrentCity("武汉");          // 设置地图显示的城市 此项是必须设置的
 	map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
@@ -102,6 +102,10 @@ function init(){
 		});
 		local.search(myValue);
 	}
+	//输入提示标签值返回
+	function G(id) {
+		return document.getElementById(id);
+	}
 }
 
 //改变地图样式
@@ -110,10 +114,7 @@ function changeMapStyle(style){
 	$('#desc').html(mapstyles[style].desc);
 }
 
-//输入提示标签值返回
-function G(id) {
-	return document.getElementById(id);
-}
+
 
 
 
@@ -146,5 +147,97 @@ function BusQuery(){
 }
 
 
-	
+//-------------------------------------------------------------------------------------------
+// 创建单点标记函数
+function addMarker(point){
+  var marker = new BMap.Marker(point);
+  map.addOverlay(marker);
+  
+}
+
+// 创建跳动标记函数
+function addMarkerBound(point){
+  var marker = new BMap.Marker(point);
+  map.addOverlay(marker);
+  marker.setAnimation(BMAP_ANIMATION_BOUNCE); //跳动的动画
+}
+
+// 创建图片标记函数
+function addMarkerPic(point){
+  var myIcon = new BMap.Icon("http://developer.baidu.com/map/jsdemo/img/fox.gif", new BMap.Size(300,157));
+  var marker = new BMap.Marker(point,{icon:myIcon});  // 创建标注
+  map.addOverlay(marker);
+}
+
+//添加小学
+function addPrimary(){
+	//清除地图覆盖物
+	map.clearOverlays();
+	//创建添加的标记点
+	var point = new BMap.Point(111.65, 40.82);
+	//定位地图中心到标记点
+	map.centerAndZoom(point, 18);
+	//调用标记点函数进行标记
+	addMarker(point);
+}
+
+//添加初中
+function addJuniormiddle(){
+	//清除地图覆盖物
+	map.clearOverlays();
+	//创建添加的标记点
+	var point = new BMap.Point(106.71, 26.57);
+	//定位地图中心到标记点
+	map.centerAndZoom(point, 18);
+	//调用标记点函数进行标记
+	addMarker(point);
+}
+
+//添加高中
+function addSeniormiddle(){
+	//清除地图覆盖物
+	map.clearOverlays();
+	//创建添加的标记点
+	var point = new BMap.Point(116.46,39.92);
+	//定位地图中心到标记点
+	map.centerAndZoom(point, 18);
+	//调用标记点函数进行标记
+	addMarkerBound(point);
+}
+
+//添加大学
+function addUniversity(){
+	//清除地图覆盖物
+	map.clearOverlays();
+	//创建添加的标记点
+	var point = new BMap.Point(114.353622,30.56486);
+	//定位地图中心到标记点
+	map.centerAndZoom(point, 18);
+	//调用标记点函数进行标记
+	addMarkerPic(point);
+}
+
+//显示全部
+function fullscreen(){
+	//清除地图覆盖物
+	map.clearOverlays();
+	//定义信息点坐标集合
+	var data_info = [[111.65, 40.82,"我的小学"],
+					 [106.71, 26.57,"我的初中"],
+					 [116.46,39.92,"我的高中"],
+					 [114.353622,30.56486,"我的大学"]
+					];
+	//遍历每个点的经纬度
+	//data_info[i][0]，每一个坐标点的第一列，即经度
+	//data_info[i][1]，每一个坐标点的第二列，即纬度
+	for (var i = 0; i < data_info.length; i ++) {
+		var marker = new BMap.Point(data_info[i][0],data_info[i][1]);
+		//调用添加标注点函数，逐个添加标记点
+		addMarker(marker);
+	}
+	//定义地图的中心点，在中国的中心，西安附近
+	var point = new BMap.Point(108.95,34.27);
+	//显示中心与地图级别，为了能够看到全国范围
+	map.centerAndZoom(point, 5);
+}
 
